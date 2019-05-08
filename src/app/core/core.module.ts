2d9a6cb4e98d9a2342ee4e189b2beb9cedf673de
Max Hill <max.hill@hiq.se>
2019-05-08 10:15:41 +0200

    Setting up http-client
    Setting up http-client so we can use it in application.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { HeaderService } from './header.service';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  providers: [HeaderService]
+  imports: [CommonModule, HttpClientModule],
+  providers: [HeaderService],
+  exports: [HttpClientModule]
 })
 export class CoreModule {}
